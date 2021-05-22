6123499333f71f0066fa76295d12b4f0b0c6e08f
Phillip Morrow <phillip.l.morrow@gmail.com>
2021-05-22 15:50:24 -0500

    created driver manager and first test fixture

---
+++ tests/end-to-end/login.py
@@ -0,0 +1,6 @@
+from utilities.webdriverconfig import SetDriver
+
+def setup_function():
+    driver = SetDriver()
+    
+
---
+++ utilities/webdriverconfig.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import FirefoxDriverManager
+from dotenv import load_dotenv
+import os
+
+driver = os.environ.get("driver")
+
+def SetDriver() :
+    if (driver == "CHROME"):
+        return webdriver.Chrome(ChromeDriverManager().install())
+    if (driver == "FIREFOX"):
+        return webdriver.Firefox(FirefoxDriverManager().install())
+    return webdriver.Remote('127.0.0.1:4444')
