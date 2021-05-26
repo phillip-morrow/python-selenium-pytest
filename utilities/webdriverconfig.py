874fda100b8d4e344e0b9a593ab53e817cd8b79d
pmorrowtron <phillip.l.morrow@gmail.com>
2021-05-26 00:03:01 -0500

    completed test with selenium wire assertion

--- utilities/webdriverconfig.py
+++ utilities/webdriverconfig.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from seleniumwire import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from dotenv import load_dotenv
