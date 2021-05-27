cd17e7597e37b8424db78d4d632f762491528218
pmorrowtron <phillip.l.morrow@gmail.com>
2021-05-26 21:16:47 -0500

    init isn't necessary, removed.  html reports now work

--- tests/end-to-end/conftest.py
+++ tests/end-to-end/conftest.py
@@ -1,5 +1,8 @@
 import pytest
+from datetime import datetime
+from pytest_html_reporter import attach
 from utilities.webdriverconfig import setdriver
+
 
 @pytest.fixture(scope="session")
 def driver_get(request):
@@ -7,6 +10,8 @@ def driver_get(request):
     session = request.node
     for item in session.items:
         cls = item.getparent(pytest.Class)
-        setattr(cls.obj,"driver", driver)
+        setattr(cls.obj, "driver", driver)
     yield
+    attach(data=driver.get_screenshot_as_png())
     driver.close()
+
