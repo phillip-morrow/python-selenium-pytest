67c7c92d23460209ded4998a139169f7aa352386
pmorrowtron <phillip.l.morrow@gmail.com>
2021-06-09 13:50:32 -0500

    swapped out os.environ.get for os.getenv

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -9,12 +9,12 @@ load_dotenv()
 
 @pytest.fixture(scope="class")
 def login():
-    domain = os.environ.get('BASE_URL')
-    port = os.environ.get('BACK_END_PORT')
+    domain = os.getenv('BASE_URL')
+    port = os.getenv('BACK_END_PORT')
     pytest.baseUrl = f'{domain}:{port}'
     payload = {
-        'username': os.environ.get('TEST_USER'),
-        'password': os.environ.get('TEST_PASS')
+        'username': os.getenv('TEST_USER'),
+        'password': os.getenv('TEST_PASS')
     }
     response = requests.post(f'{pytest.baseUrl}/login', data=payload)
     pytest.cookies = response.cookies
